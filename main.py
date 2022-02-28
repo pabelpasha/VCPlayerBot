d335e20b3b5147f52173fab33d3b2f3d07a6d25d
Mariaa74 <90863831+Mariaa74@users.noreply.github.com>
2022-02-28 12:05:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def main():
                 msg=await db.get_config("RESTART")
                 if msg:
                     try:
-                        k=await bot.edit_message_text(msg['chat_id'], msg['msg_id'], text="Succesfully restarted. Join @Mirrorcloudbd")
+                        k=await bot.edit_message_text(msg['chat_id'], msg['msg_id'], text="Succesfully restarted.")
                         await db.del_config("RESTART")
                     except:
                         pass
